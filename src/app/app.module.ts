abf30edbcfa4a258e9df85d753510f17fb8db4cb
Sourabh Verma <sourabhverma4374@gmail.com>
2018-01-06 08:24:54 +0530

    First set of modifications. adding themeon related files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
